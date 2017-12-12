8dfdf390c26b81d7c1a7a0c92675631363b15523
SirB31415 <31518716+SirB31415@users.noreply.github.com>
2017-12-12 12:59:10 -0700

    Made a runner and start method in the controller.

--- src/poke/controller/PokeRunner.java
+++ src/poke/controller/PokeRunner.java
@@ -2,5 +2,9 @@ package poke.controller;
 
 public class PokeRunner
 {
-
+	public static void main(String [] args)
+	{
+		PokeController app = new PokeController();
+		app.start();
+	}
 }
--- src/poke/view/PokePanel.java
+++ src/poke/view/PokePanel.java
@@ -2,5 +2,5 @@ package poke.view;
 
 public class PokePanel
 {
-
+	
 }
