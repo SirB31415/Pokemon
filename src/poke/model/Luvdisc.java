f4be2d68460a8b0e2992e4a20a1a85d5c1b6ade5
SirB31415 <31518716+SirB31415@users.noreply.github.com>
2017-12-18 12:34:18 -0700

    finished luvdisc.

--- src/poke/model/Luvdisc.java
+++ src/poke/model/Luvdisc.java
@@ -6,5 +6,26 @@ public class Luvdisc extends Pokemon implements Water
 	{
 		super(number, name);
 	}
-	public 
+	public String sweetKiss(int sweetKisses)
+	{
+		if (sweetKisses == 69)
+		{
+			return "ohohohoh naughty naughty";
+		}
+		else
+		{
+			return "that's not the magic number";
+		}
+	}
+	public String drainingKiss(int drainKisses)
+	{
+		if (drainKisses == 420)
+		{
+			return "verrrrrryyyyy mature";
+		}
+		else
+		{
+			return "i shall drain the life out of you, **auto wins round**";
+		}
+	}
 }
