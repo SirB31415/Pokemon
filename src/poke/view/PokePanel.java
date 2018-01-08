44a7f7964436add174fedf03f479c4bcec8863f0
SirB31415 <31518716+SirB31415@users.noreply.github.com>
2018-01-08 12:58:37 -0700

    Added images, added most of the panel class, added a listener in the panel class, changed controller.

--- src/poke/view/PokePanel.java
+++ src/poke/view/PokePanel.java
@@ -1,6 +1,44 @@
 package poke.view;
 
-public class PokePanel
+import poke.model.Pokemon;
+import poke.controller.PokeController;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JCheckBox;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+
+public class PokePanel extends JPanel
 {
+	private PokeController appController;
+	private SpringLayout appLayout;
 	
+	private JLabel healthLabel;
+	private JLabel attackLabel;
+	private JLabel nameLabel;
+	private JLabel numberLabel;
+	private JLabel evolveableLabel;
+	private JLabel modifierLabel;
+	private JLabel iconLabel;
+	
+	private JCheckBox evolvableBox;
+	private JTextField nameField; 
+	private JTextField numberField;
+	private JTextField attackField;
+	private JTextField healthField;
+	private JTextField modifierField;
+	
+	private JTextArea descriptionArea;
+	private JTextArea typeArea;
+	
+	private JButton clearButton;
+	private JButton saveButton;
+	private JComboBox pokedexDropDown;
+	
+	private JPanel firstType;
+	private JPanel secondType;
+	private JPanel thirdType;
+	private JPanel fourthType;
 }
